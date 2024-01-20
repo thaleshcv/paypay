e347e35c13a0aed8954573543db0be81d0fe3320
thaleshcv <thalesherbert@gmail.com>
2024-01-20 15:29:30 -0300

    fixing specs

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Dashboards", type: :request do
+  before { sign_in(FactoryBot.create(:user)) }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/dashboard/index"
+      get "/dashboard"
       expect(response).to have_http_status(:success)
     end
   end
