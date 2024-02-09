160dd3835f8fc5bf5fb12dce207dd9b6dcde6e1e
thaleshcv <thalesherbert@gmail.com>
2024-02-09 09:50:59 -0300

    checkpoint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,7 @@ module ApplicationHelper
   end
 
   def user_avatar(user)
-    content_tag :span,
-      user.email[0].upcase,
-      style: "width:1.5rem;height:1.5rem;",
-      class: "rounded-circle d-flex align-items-center justify-content-center bg-dark text-light"
+    content_tag :span, user.email[0].upcase, class: "user-avatar"
   end
 
   def dismiss_button(target)
