b0a48656bebbcbb4b6f8446befb93441f4abebce
thaleshcv <thalesherbert@gmail.com>
2024-02-13 19:58:10 -0300

    checkpoint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,6 +5,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  def new
+    render_not_found
+  end
+
   def show; end
 
   def update
