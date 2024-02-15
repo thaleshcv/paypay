5f9936eb7edb5cff839fb24c08f2aec93af3a4ae
thaleshcv <thalesherbert@gmail.com>
2024-02-15 19:00:16 -0300

    update README; fix application_query;

--- app/models/application_query.rb
+++ app/models/application_query.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 # Base class for query objects.
-module Queries
-  class ApplicationQuery
-    include ActiveModel::Model
-    include ActiveModel::Attributes
-    include ActiveModel::AttributeAssignment
+class ApplicationQuery
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActiveModel::AttributeAssignment
 
-    def perform
-      raise NotImplementedError, "Method +perform+ not implemented."
-    end
+  def perform
+    raise NotImplementedError, "Method +perform+ not implemented."
   end
 end
