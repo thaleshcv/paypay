c30287268dc70b75b25322a0a9310f3d29ac89ee
thaleshcv <thalesherbert@gmail.com>
2024-02-17 15:10:07 -0300

    removing auto-update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,3 @@
 every 1.day, at: "0:03 am" do
   runner "CreateEntriesForActiveBillingsJob.perform_now"
 end
-
-every 1.hour do
-  command "/var/apps/paypay/bin/self-update > /var/logs/paypay-self-update.log"
-end
