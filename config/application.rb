3e203343f088b92b63264b89bfd1cad78c3411a9
thaleshcv <thalesherbert@gmail.com>
2024-02-18 14:11:33 -0300

    disabling rack-attack

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Rack::Attack.enabled = false
+
 module Paypay
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
