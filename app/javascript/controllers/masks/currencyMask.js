00eb7a9ccdaf5b8b335aa4ac917d951b816c5839
thaleshcv <thalesherbert@gmail.com>
2024-02-20 09:37:30 -0300

    fixing currency mask

--- app/javascript/controllers/masks/currencyMask.js
+++ app/javascript/controllers/masks/currencyMask.js
@@ -1,13 +1,12 @@
 export default function currencyMask(value) {
-  let digits = Number(String(value).replace(/\D/g, ""));
-
-  if (!digits || digits === 0) {
+  const digits = Number(value).toFixed(2).replace(/\D/g, "");
+  if (!digits || Number(digits) === 0) {
     return "";
   }
 
   return (
     "$ " +
-    String(digits)
+    digits
       .padStart(3, "0")
       .replace(/(\d+)(\d{2})/g, "$1,$2")
       .replace(/(\d)(?=(\d{3})+\,)/g, "$1.")
