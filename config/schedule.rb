d553cfef9e495221501ab8342d255e85827034f6
thaleshcv <thalesherbert@gmail.com>
2024-03-02 10:42:39 -0300

    updating schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-every 1.day, at: "0:03 am" do
+every 1.day, at: "3:33 am" do
   runner "CreateEntriesForActiveBillingsJob.perform_now"
+end
+
+every 1.day, at: "23:59 am" do
+  command "cd /var/apps/paypay; bin/self-update"
 end
